20e1aa5385a1b5c3fd758d0e89f5d5975c78414c
Harprit Sumon <harpritsmn32@yahoo.de>
2020-06-10 10:20:33 +0200

    added exercise one part one

---
+++ basic/scriptOne.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+		$('.one').on('click', function() {
+            $('#result').html('You clicked on the Christmas bell')
+        })
+
+        $('.two').on('click', function() {
+            $('#result').html('You clicked on the Christmas gift')
+        })
+
+        $('.three').on('click', function() {
+            $('#result').html('You clicked on santa')
+        })
+
+    });
