87fd65983b05d926eabb3acfe4c0f515d6a7ae43
Harprit Sumon <harpritsmn32@yahoo.de>
2020-06-10 11:05:50 +0200

    finished basic exercise

---
+++ basic/scriptTwo.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+		$(".santaOne").click(function(){
+			$(".santaOne").hide();
+		});
+
+		$(".santaTwo").click(function(){
+			$(".santaTwo").hide();
+		});
+
+		$(".santaThree").click(function(){
+			$(".santaThree").hide();
+		});
+
+		$(".santaFour").click(function(){
+			$(".santaFour").hide();
+		});
+
+    });
